9f2e21f0fd89c9465a01967ed2307f5af6b51deb
sasuske <jelver00@hotmail.com>
2022-11-09 11:47:42 -0500

    update changes

--- controllers/Users/userSearchController.js
+++ controllers/Users/userSearchController.js
@@ -1,25 +1,27 @@
-const createHttpError = require('http-errors')
-const { User } = require('../../database/models')
-const { endpointResponse } = require('../../helpers/success')
-const { catchAsync } = require('../../helpers/catchAsync')
+const createHttpError = require("http-errors");
+const { User } = require("../../database/models");
+const { endpointResponse } = require("../../helpers/success");
+const { catchAsync } = require("../../helpers/catchAsync");
 
 //  get all users controller and specifics attributes return
 module.exports = {
   getAllUsers: catchAsync(async (req, res, next) => {
     try {
       //called to the db and only return specifics attributes
-      const response = await User.findAll({attributes:['firstName','lastName','email','createdAt']})
+      const response = await User.findAll({
+        attributes: ["firstName", "lastname", "email", "createdAt"],
+      });
       endpointResponse({
         res,
-        message: 'Users obtained',
+        message: "Users obtained",
         body: response,
-      })
+      });
     } catch (error) {
       const httpError = createHttpError(
         error.statusCode,
-        `[Error obtaining UserSearch] - [UserSearchController - GET]: ${error.message}`,
-      )
-      next(httpError)
+        `[Error obtaining UserSearch] - [UserSearchController - GET]: ${error.message}`
+      );
+      next(httpError);
     }
   }),
-}
+};
--- controllers/auth/loginController.js
+++ controllers/auth/loginController.js
@@ -2,26 +2,36 @@ const createHttpError = require("http-errors");
 const { catchAsync } = require("../../helpers/catchAsync");
 const { endpointResponse } = require("../../helpers/success");
 const { ErrorObject } = require("../../helpers/error");
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken')
-const dotenv = require('dotenv');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 const { User } = require("../../database/models");
-dotenv.config();
 
 module.exports = {
   loginUser: catchAsync(async (req, res, next) => {
     try {
       const user = await User.findOne({ where: { email: req.body.email } });
-      if (!user) throw new ErrorObject(`error user with email ${req.body.email} does not exist`, 404);
+      if (!user)
+        throw new ErrorObject(
+          `error user with email ${req.body.email} does not exist`,
+          404
+        );
 
-      const validatePass = await bcrypt.compare(req.body.password, user.password)
-      if(!validatePass) throw new ErrorObject("You have entered an invalid password", 404);
-      const token = jwt.sign({ id: user.id }, process.env.JWT_PASS, {expiresIn: '1h'})
+      const validatePass = await bcrypt.compare(
+        req.body.password,
+        user.password
+      );
+      if (!validatePass)
+        throw new ErrorObject("You have entered an invalid password", 404);
+      const token = jwt.sign({ id: user.id }, process.env.JWT_PASS, {
+        expiresIn: "1h",
+      });
 
       endpointResponse({
         res,
         message: "login successfully",
-        body: {user,token},
+        body: { user, token },
       });
     } catch (error) {
       const httpError = createHttpError(
